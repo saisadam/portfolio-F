783be2105f47a9a6f884e3f529f3fc2f16bb210f
saisadam <saisadam.in@gmail.com>
2026-08-09 12:33:59 +0530

    front end

---
+++ public/script.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("contactForm");
+const successMessage = document.getElementById("success-message");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const data = {
+        name: form.name.value,
+        email: form.email.value,
+        subject: form.subject.value,
+        message: form.message.value
+    };
+
+    try {
+
+        
+
+        const response = await fetch("https://portfolio-xhve.onrender.com/contact", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+});
+
+        const result = await response.json();
+
+        if (result.success) {
+
+            form.reset();
+            successMessage.style.display = "block";
+
+        } else {
+
+            alert(result.message);
+
+        }
+
+    } catch (err) {
+
+        console.error(err);
+        alert("Something went wrong.");
+
+    }
+
+});
